76856a50eb8cb6375d13ec57c164b62225a0d6de
gabrieladrianofx <gabrieladrianofx@gmail.com>
2024-08-18 23:53:28 -0300

    HEY-7: it should have at least 10 characters when creating new question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -10,7 +10,7 @@ class QuestionController extends Controller
     public function store(): RedirectResponse
     {
         Question::query()->create(request()->validate([
-            'question' => ['required'],
+            'question' => ['required', 'min:10'],
         ]));
 
         return to_route('dashboard');
