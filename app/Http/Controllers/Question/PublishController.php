e0e7f19b39a218afea7f3614a901ade97ea7f050
gabrieladrianofx <gabrieladrianofx@gmail.com>
2024-08-20 23:36:55 -0300

    hey-12: it should make sure that only the person who has created the question can publish the question

--- app/Http/Controllers/Question/PublishController.php
+++ app/Http/Controllers/Question/PublishController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers\Question;
 use App\Http\Controllers\Controller;
 use App\Models\Question;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpFoundation\Response;
 
 class PublishController extends Controller
 {
     public function __invoke(Question $question): RedirectResponse
     {
+        Gate::authorize('publish', $question);
+        // abort_unless(user()->can('publish', $question), Response::HTTP_FORBIDDEN);
+
         $question->update(['draft' => false]);
 
         return back();
