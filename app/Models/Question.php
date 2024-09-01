1707ebe4a9b193ff4db77fb9d8a3ef478ae1c1a5
gabrieladrianofx <gabrieladrianofx@gmail.com>
2024-09-01 16:02:53 -0300

    hey-18: it should be able to archive a question using soft delete

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Database\Factories\QuestionFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany};
+use Illuminate\Database\Eloquent\{Model, SoftDeletes};
 
 class Question extends Model
 {
     /** @use HasFactory<QuestionFactory> */
     use HasFactory;
+    use SoftDeletes;
 
     /**
      * @var array<string, string>
